b41246bb57d58c2f82fcbf0dfd3b5e3c63163ca5
Paul Moreland <Paul.Moreland@edu.lidkoping.se>
2022-04-25 14:40:58 +0200

    Add project files.

---
+++ list-to-file/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace list_to_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/PAMO001/Desktop/scores.txt";
+
+            List<string> scores = new List<string>();
+
+            scores.Add("10");
+            scores.Add("40");
+            scores.Add("42");
+
+            File.WriteAllLines(path, scores);
+
+            string[] readScores = File.ReadAllLines(path);
+
+            List<string> scoreList = new List<string>(readScores);
+
+            foreach (string score in scoreList)
+            {
+                Console.WriteLine(score);
+            }
+        }
+    }
+}
